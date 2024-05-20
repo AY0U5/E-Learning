d84215b20a817a4886c2fe7bcdddb1f6a4213dd8
Adel <adellarf002@gmail.com>
2024-05-20 11:29:05 +0100

    backend all

--- src/main/java/ma/zs/alc/zynerator/security/config/WebSecurityConfig.java
+++ src/main/java/ma/zs/alc/zynerator/security/config/WebSecurityConfig.java
@@ -69,7 +69,7 @@ public class WebSecurityConfig {
                                 .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll() // Permit access to Swagger UI and API docs
                                 .requestMatchers("/api/admin/login").permitAll()
                                 .requestMatchers("/api/admin/**").permitAll()//.hasAnyAuthority(AuthoritiesConstants.ADMIN)
-                                .requestMatchers("/api/admin/**").hasAnyAuthority(AuthoritiesConstants.ANONYMOUS)
+                                .requestMatchers("/api/admin/**").permitAll()//.hasAnyAuthority(AuthoritiesConstants.ANONYMOUS)
                                 .anyRequest().authenticated()
 
                 );
